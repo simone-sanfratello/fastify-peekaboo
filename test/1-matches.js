efdc276be8d3aa8c1b5b1ab22d719ae666c6c49e
Simone Sanfratello <simone@braceslab.com>
2018-09-28 06:46:40 +0200

    wip: test suite

--- test/1-matches.js
+++ test/1-matches.js
@@ -3,12 +3,13 @@ const fastify = require('fastify')
 const got = require('got')
 
 const peekaboo = require('../src/plugin')
+const helper = require('./helper')
 
 tap.test('peekaboo match by custom rule',
   async (_test) => {
     _test.plan(1)
-    const _fastify = fastify()
-    _fastify.register(peekaboo, {
+
+    const _fastify = helper.init(peekaboo, {
       xheader: true,
       match: {
         [peekaboo.MATCH.CUSTOM]: () => {
@@ -22,22 +23,22 @@ tap.test('peekaboo match by custom rule',
     })
 
     try {
-      await _fastify.listen(0)
-      _fastify.server.unref()
-      const _port = _fastify.server.address().port
-      const _url = `http://127.0.0.1:${_port}/home`
-      await got(_url)
-      const _response = await got(_url)
+      const _response = await helper.request(_fastify, {
+        url: '/home',
+        method: 'get'
+      })
       if (!_response.headers['x-peekaboo']) {
         _test.fail()
       }
-      _fastify.close()
-      _test.pass()
     } catch (error) {
       _test.threw(error)
     }
+
+    _fastify.close()
+    _test.pass()
   })
 
+  /*
 tap.test('peekaboo match by method, url and querystring',
   async (_test) => {
     _test.plan(1)
@@ -70,3 +71,4 @@ tap.test('peekaboo match by method, url and querystring',
       _test.threw(error)
     }
   })
+*/
