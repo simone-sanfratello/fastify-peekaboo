076840edebc7d27eb448476aef842dfb1569f4a5
Simone Sanfratello <simone@braceslab.com>
2018-12-27 07:09:50 +0100

    wip: test request query

--- test/4-match-request-query.js
+++ test/4-match-request-query.js
@@ -150,3 +150,64 @@ tap.test('peekaboo matching by request query (array)',
       _test.threw(error)
     }
   })
+
+tap.test('peekaboo matching by request query (function)',
+  async (_test) => {
+    _test.plan(1)
+    const _fastify = fastify()
+    _fastify
+      .register(peekaboo, {
+        xheader: true,
+        matches: [{
+          request: {
+            route: '/query',
+            query: function (query) {
+              return parseInt(query.page) > 0
+            }
+          }
+        }]
+      })
+
+    _fastify.all('/query', async (request, response) => {
+      response.send(JSON.stringify(request.query))
+    })
+
+    try {
+      await _fastify.listen(0)
+      _fastify.server.unref()
+      const _port = _fastify.server.address().port
+
+      let _url = `http://127.0.0.1:${_port}/query?page=0`
+      await got(_url)
+      let _response = await got(_url)
+      if (_response.headers['x-peekaboo']) {
+        _test.fail()
+      }
+
+      _url = `http://127.0.0.1:${_port}/query?page=1&offset=2`
+      await got(_url)
+      _response = await got(_url)
+      if (!_response.headers['x-peekaboo']) {
+        _test.fail()
+      }
+
+      _url = `http://127.0.0.1:${_port}/query?page=2&offset=2&filter=value`
+      await got(_url)
+      _response = await got(_url)
+      if (!_response.headers['x-peekaboo']) {
+        _test.fail()
+      }
+
+      _url = `http://127.0.0.1:${_port}/query?offset=0`
+      await got(_url)
+      _response = await got(_url)
+      if (_response.headers['x-peekaboo']) {
+        _test.fail()
+      }
+
+      _fastify.close()
+      _test.pass()
+    } catch (error) {
+      _test.threw(error)
+    }
+  })
